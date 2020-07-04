b0bf78d311338d71113d98fcbdccd9ba3224f42c
Abbosbek Muydinboev <muydinboevabbosbek@gmail.com>
2020-07-04 10:16:14 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -18,7 +18,9 @@ end
 
 def find_valid_calls(planeteer_calls)
   valid_calls = ["Earth!", "Wind!", "Fire!", "Water!", "Heart!"]
-  planeteer_calls.find do |i|
-    i 
+  for i in valid_calls do
+    planeteer_calls.find do |n|
+      n == i
+    end
   end
 end
